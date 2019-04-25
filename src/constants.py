71f7127d5301d052b791f53d5e31239bd7c32e66
fgrimau <florent.grimau@ulb.be>
2019-04-25 11:19:53 +0200

    Added the most useful feature

--- src/constants.py
+++ src/constants.py
@@ -1,5 +1,8 @@
+import random
 import pygame
 from pygame.locals import *
+
+
 from functions import *
 from db import *
 
@@ -9,9 +12,16 @@ SCREEN_Y = 768
 screen = pygame.display.set_mode((SCREEN_X, SCREEN_Y))
 
 #FONTS
+
+if random.random() < 0.33:
+    fontToLoad = 'fonts/comic-sans-ms.ttf'
+else:
+    fontToLoad = 'fonts/BebasNeue-Regular.ttf'
+
+
 fonts = {}
 for x in [14, 20, 25, 30, 35, 50, 75, 100]:
-    fonts[str(x)] = pygame.font.Font('fonts/BebasNeue-Regular.ttf', x)
+    fonts[str(x)] = pygame.font.Font(fontToLoad, x)
 
 urlabBanner = CreateBanner(fonts, SCREEN_X)
 
